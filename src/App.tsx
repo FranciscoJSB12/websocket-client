fee1a354813f38d9f8fd2cde00a9522e2078036b
FranciscoJSB12 <javif8@gmail.com>
2024-05-05 07:56:48 -0400

    feat: Add POO in socket client for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,56 @@
 import { useEffect, useState } from 'react';
-import { connectToServer } from './socket-client';
-
-const socket = connectToServer();
+import {
+  ManageConnection,
+  SocketIoConnection,
+  MessageReceivedListener,
+} from './socket-client';
 
 function App() {
   const [state, setState] = useState('Diconnected');
   const [clients, setClients] = useState<string[]>([]);
   const [formValue, setFormValue] = useState('');
+  const [token, setToken] = useState('');
   const [messages, setMessages] = useState<string[]>([]);
+  const [socket, setSocket] = useState<SocketIoConnection | null>(null);
+
+  const onMessageReceived: MessageReceivedListener = payload => {
+    setMessages(prevMessage => [
+      ...prevMessage,
+      `${payload.fullName}: ${payload.message}`,
+    ]);
+  };
 
   useEffect(() => {
-    socket.on('connect', () => {
-      setState('Connected');
-    });
+    if (socket) {
+      socket.addListeners(
+        () => setState('Connected'),
+        () => setState('Diconnected'),
+        () => setClients([...clients]),
+        onMessageReceived
+      );
+    }
+  }, [socket]);
 
-    socket.on('disconnect', () => {
-      setState('Diconnected');
-    });
+  const submitTokenHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    socket.on('clients-updated', (clients: string[]) => {
-      setClients([...clients]);
-    });
+    if (token === '') return;
 
-    socket.on(
-      'message-from-server',
-      (payload: { fullName: string; message: string }) => {
-        setMessages(prevMessage => [
-          ...prevMessage,
-          `${payload.fullName}: ${payload.message}`,
-        ]);
-      }
-    );
-  }, []);
+    const newConnection = new ManageConnection(token, '/').connectToServer();
 
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    const newSocket = new SocketIoConnection(newConnection);
+
+    setSocket(newSocket);
+
+    setToken('');
+  };
+
+  const submitMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (formValue === '') return;
 
-    socket.emit('message-from-client', {
-      id: 'YO',
-      message: formValue,
-    });
+    socket && socket.emitMessage(formValue);
 
     setFormValue('');
   };
@@ -50,13 +59,24 @@ function App() {
     <main>
       <h1>Websocket</h1>
       <h2>Status: {state}</h2>
+      <hr />
+      <h2>Insert token:</h2>
+      <form onSubmit={submitTokenHandler}>
+        <input
+          type='text'
+          onChange={e => setToken(e.currentTarget.value)}
+          value={token}
+        />
+        <button type='submit'>Send</button>
+      </form>
       <ul>
         {clients.map((client, index) => (
           <li key={index}>{client}</li>
         ))}
       </ul>
       <hr />
-      <form onSubmit={submitHandler}>
+      <h2>Type your message</h2>
+      <form onSubmit={submitMessageHandler}>
         <input
           type='text'
           onChange={e => setFormValue(e.currentTarget.value)}
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,13 +1,56 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client";
 
-export const connectToServer = () => {
-    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
-        extraHeaders: {
-            authentication: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgzYmVlMzhiLTVkYzAtNGViNC1iZjI4LTAxYjQyN2FkMTllZCIsImlhdCI6MTcxNDc3MzExOCwiZXhwIjoxNzE0NzgwMzE4fQ.AEUV-d_ncBFor77KsDDzqgIQG3k0LuZQNwbBhcaBHI0'
-        }
-    });
+export type CommonListener = () => void;
 
-    const socket = manager.socket('/');
+export interface MessageReceivedListenerPayload {
+    fullName: string; 
+    message: string;
+}
 
-    return socket;
+export type MessageReceivedListener = (payload: MessageReceivedListenerPayload) => void;
+
+/*const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+        authentication: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgzYmVlMzhiLTVkYzAtNGViNC1iZjI4LTAxYjQyN2FkMTllZCIsImlhdCI6MTcxNDg3NjU0MywiZXhwIjoxNzE0ODgzNzQzfQ.2n_3O3a4vLV4A82Ni9RNq7CGCwRQ5OCyCks3cSFZ8c0'
+    }
+});*/
+
+export class ManageConnection {
+    constructor(
+        private readonly token: string,
+        private readonly nameSpace: string,
+    ){}
+
+    connectToServer() {
+        const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+            extraHeaders: {
+                authentication: this.token,
+            }   
+        });
+
+        return manager.socket(this.nameSpace);
+        //Al aplicar manager.socker('/') obtenemos nuestro socket, que es la comunicación con nuestro servidor)
+    }
+}
+
+export class SocketIoConnection {
+    constructor(
+        private readonly socket: Socket,
+    ) {}
+
+    addListeners(
+        onConnect: CommonListener, 
+        onDisconnect: CommonListener, 
+        onClientUpdated: CommonListener,
+        onMessageReceived: MessageReceivedListener
+    ) {
+        this.socket.on('connect', onConnect);
+        this.socket.on('disconnect', onDisconnect);
+        this.socket.on('clients-updated', onClientUpdated);
+        this.socket.on('message-from-server', onMessageReceived);
+    }
+
+    emitMessage (payload: string) {
+        this.socket.emit('message-from-client', { id: 'YO', message: payload});
+    }
 }
